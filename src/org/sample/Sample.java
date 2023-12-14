8b4c9812efbd2dba27f85e6dd55ad692b0dafe1b
ramkumar090986 <ramkumar8687@gmail.com>
2023-12-14 14:45:50 +0530

    Factorial program

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		int fact=1,n=5;
+		
+		for (int i = 1; i <=5; i++) {
+			
+			fact=fact*i;
+			
+		}
+		
+		System.out.println("Factorial of a number:"+fact);
+				
+		
+	}
+
+}
